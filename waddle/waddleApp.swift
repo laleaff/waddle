63e7107dec2c3e80c5f649876222c75c133976a3
Alifa <liffa.rr@gmail.com>
2025-04-07 14:26:10 +0700

    Initial Commit

---
+++ waddle/waddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  waddleApp.swift
+//  waddle
+//
+//  Created by Alifa Reppawali on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct waddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //ContentView()
+        }
+    }
+}
---
+++ waddleTests/waddleTests.swift
@@ -0,0 +1,17 @@
+//
+//  waddleTests.swift
+//  waddleTests
+//
+//  Created by Alifa Reppawali on 26/03/25.
+//
+
+import Testing
+@testable import waddle
+
+struct waddleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
